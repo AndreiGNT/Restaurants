c75520c5b1a7df04da592ad493beab5f7400aa1a
AndreiGNT <aagirnet@gmail.com>
2026-01-12 23:29:42 +0200

    Add project files.

---
+++ Restaurants.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IWeatherForecastService _weatherForecastService;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger,
+        IWeatherForecastService weatherForecastService)
+    {
+        _logger = logger;
+        _weatherForecastService = weatherForecastService;
+    }
+
+    [HttpGet]
+    [Route("example")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var result = _weatherForecastService.Get();
+        return result;
+    }
+
+    [HttpGet]
+    [Route("{take}/currentDay")]
+    public IActionResult Get([FromQuery] int max, [FromRoute] int take)
+    {
+        var result = _weatherForecastService.Get().First();
+
+        //Response.StatusCode = 400;
+        //  return StatusCode(400, result);
+        return NotFound(result);
+    }
+
+    [HttpPost]
+    public string Hello([FromBody] string name)
+    {
+        return $"Hello {name}";
+    }
+}
