93d0b7591004a1a5fb3fd0f5da9948c038c47120
AndreiGNT <aagirnet@gmail.com>
2026-02-22 10:35:02 +0200

    Adding Swagger

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 using Restaurants.Application.Restaurants.Commands.DeleteRestaurant;
 using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
+using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Application.Restaurants.Queries.GetRestaurantById;
 
@@ -13,14 +14,14 @@ namespace Restaurants.API.Controllers;
 public class RestaurantsController( IMediator mediator ) : ControllerBase
 {
     [HttpGet]
-    public async Task<IActionResult> GetAll()
+    public async Task<ActionResult<IEnumerable<RestaurantDto>>> GetAll()
     {
         var retaurants = await mediator.Send(new GetAllRestaurantsQuery());
         return Ok(retaurants);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetById([FromRoute]int id)
+    public async Task<ActionResult<RestaurantDto?>> GetById([FromRoute]int id)
     {
         var retaurant = await mediator.Send(new GetRestaurantByIdQuery(id));
 
@@ -41,6 +42,8 @@ public class RestaurantsController( IMediator mediator ) : ControllerBase
     }
 
     [HttpPatch("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateRestaurant([FromRoute]int id, [FromBody] UpdateRestaurantCommand command)
     {
         command.Id = id;
@@ -55,6 +58,8 @@ public class RestaurantsController( IMediator mediator ) : ControllerBase
     }
 
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteRestaurant([FromRoute] int id)
     {
         var isDeleted = await mediator.Send(new DeleteRestaurantCommand(id));
