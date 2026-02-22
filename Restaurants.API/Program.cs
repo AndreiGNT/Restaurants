2d4f04ccea18696068d3d931659f3e382e80af9d
AndreiGNT <aagirnet@gmail.com>
2026-02-22 10:08:22 +0200

    Adding logging to a file.

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -3,6 +3,7 @@ using Restaurants.Infrastructure.Seeder;
 using Restaurants.Application.Extensions;
 using Serilog;
 using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Host.UseSerilog((context, configuration) =>
-    configuration
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine} {Message:lj}{NewLine}{Exception}")
+    configuration.ReadFrom.Configuration(context.Configuration)
 );
 
 var app = builder.Build();
