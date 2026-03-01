e9ca0ccb2fec8d35a235da3c453b6a0a5f133135
AndreiGNT <aagirnet@gmail.com>
2026-03-01 22:20:59 +0200

    New API Create Dish for a particular Restaurant.

---
+++ Restaurants.API/Controllers/DishController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Dishes.Commands.CreateDish;
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("api/restaurants/{restaurntId}/dishes")]
+public class DishController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateDish([FromRoute]int restaurntId, CreateDishCommand command)
+    {
+        command.RestaurantId = restaurntId;
+
+        await mediator.Send(command);
+        return Created();
+    }
+}
