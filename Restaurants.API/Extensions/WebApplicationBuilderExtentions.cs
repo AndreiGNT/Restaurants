c5f70aeeabbc7738b22f8d1645389d68f1b4a5df
AndreiGNT <aagirnet@gmail.com>
2026-03-14 17:11:47 +0200

    I have extracted the presentation layer services. Restructured the code.

---
+++ Restaurants.API/Extensions/WebApplicationBuilderExtentions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+using Restaurants.API.Middlewares;
+using Serilog;
+
+namespace Restaurants.API.Extensions;
+
+public static class WebApplicationBuilderExtentions
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "bearerAuth"
+                }
+            },
+            []
+        }
+    });
+        });
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddScoped<LogExecutedTimeMiddleware>();
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration)
+);
+    }
+}
